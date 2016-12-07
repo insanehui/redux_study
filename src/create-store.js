f421a998f7c688ab6e16d8ac46bc7388f9dedcf3
insanehui <269200808@qq.com>
2016-12-07 22:32:35 +0800

    注释

--- src/create-store.js
+++ src/create-store.js
@@ -3,22 +3,27 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 
 import promiseMiddleware from './promise-middleware'
 
-// 在本应用中我们仅有一个 reducer,
-// 但是下面用 ES6 写的 import，有趣且一气呵成的导入并生成了多个 reducer。
-// 去 ./reducers.js 看看我们的 reducer 究竟是怎么做的(这里没有魔法)。
 import * as reducers from './reducers'
 
-// 这里看到的 data 参数是用于初始化 Redux store。
-// 为简单起见我们不讨论它，但要感谢它让你在有真实数据的情况下来初始化 reducer。
-// 例如在一个同构/通用应用中，你能从服务器端拉取数据，然后序列化并传递到客户端,
-// 你的 Redux store 就能用这些数据来初始化。
-// 这里我们没传任何数据，但最好要知道这个 createStore 是做什么的
+// 以下写法是错的：
+// import reducers from './reducers'
+// 因为 ./reducers 导出形式是 export function _time(...){...}
+// 上面正确的写法，使得 reducers 为
+// {
+//  _time : function(...){...}
+// }
+// 这样可以直接用于 reducer的combine
+//
+// 而错误的写法，使得到的reducers是一个函数，则不能用于combine
+
+
 export default function(data) {
   var reducer = combineReducers(reducers)
+
+  // 使用中间件
   var finalCreateStore = applyMiddleware(promiseMiddleware)(createStore)
+
   var store = finalCreateStore(reducer, data)
 
   return store
 }
-
-// 转到 ./application.jsx，去学习使用 Provider 组件首次将 Redux 绑定到 React。
